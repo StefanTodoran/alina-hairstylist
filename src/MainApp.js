94d37551db0c042ba7bc56ccffc4394a8379c300
StefanTodoran <stefan.alex4@gmail.com>
2022-05-18 21:52:22 -0700

    added new images, made images be dynamically grabbed from the services folder

--- src/MainApp.js
+++ src/MainApp.js
@@ -71,7 +71,7 @@ export class MainApp extends Component {
       )
     }
 
-    const shapesColor = "#85a777";
+    const shapesColor = "#505D3F";
     return (
       <>
         <Header scrollCallback={this.scrollTo} mobile={this.state.portrait}/>
@@ -79,7 +79,7 @@ export class MainApp extends Component {
 
         <div className={"banner"}>
 
-          <div className="left-shapes">
+          <div className="left-shapes mobile-visible">
             <svg width="20vmin" height="20vmin" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
               <g id="Circles 1">
                 <path id="Semi Circle" d="M100.902 29C81.4019 33 80.9019 68.5 100.902 72" stroke="#fff"/>
@@ -89,7 +89,7 @@ export class MainApp extends Component {
               </g>
             </svg>
           </div>
-          <div className="right-shapes">
+          <div className="right-shapes mobile-visible">
             <svg width="20vmin" height="20vmin" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
               <rect id="Little Square" x="0.300183" y="0.643787" width="10.8505" height="10.9803"
                     transform="matrix(0.938111 0.346336 -0.337744 0.941238 75.2823 53.9339)" stroke="#fff"/>
@@ -252,13 +252,13 @@ export class MainApp extends Component {
               <b><a
                 href={"https://www.google.com/maps/dir/?api=1&destination=130%205th%20Ave%20S%20Edmonds%20WA%2098020"}
                 target={"_blank"}>Get Directions</a></b><br/><br/>
-              Monday 10:00 am - 7:00 pm<br/>
-              Tuesday 10:00 am - 7:00 pm<br/>
+              Monday    10:00 am - 7:00 pm<br/>
+              Tuesday   10:00 am - 7:00 pm<br/>
               Wednesday 10:00 am - 7:00 pm<br/>
-              Thursday 10:00 am - 7:00 pm<br/>
-              Friday 10:00 am - 7:00 pm<br/>
-              Saturday 10:00 am - 7:00 pm<br/>
-              Sunday 11:00 am - 5:00 pm
+              Thursday  10:00 am - 7:00 pm<br/>
+              Friday    10:00 am - 7:00 pm<br/>
+              Saturday  10:00 am - 7:00 pm<br/>
+              Sunday    11:00 am - 5:00 pm
             </p>
           </section>
 
@@ -276,7 +276,7 @@ export class MainApp extends Component {
                 <a href={"https://www.instagram.com/invites/contact/?i=w1cqx4msy1f0&utm_content=nrbpeaj"}
                    target={"_blank"}>
                   <svg className={"media-icon"} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <g clip-path="url(#clip0_0_1)">
+                    <g clipPath="url(#clip0_0_1)">
                       <path
                         d="M12 2.163C15.204 2.163 15.584 2.175 16.85 2.233C20.102 2.381 21.621 3.924 21.769 7.152C21.827 8.417 21.838 8.797 21.838 12.001C21.838 15.206 21.826 15.585 21.769 16.85C21.62 20.075 20.105 21.621 16.85 21.769C15.584 21.827 15.206 21.839 12 21.839C8.796 21.839 8.416 21.827 7.151 21.769C3.891 21.62 2.38 20.07 2.232 16.849C2.174 15.584 2.162 15.205 2.162 12C2.162 8.796 2.175 8.417 2.232 7.151C2.381 3.924 3.896 2.38 7.151 2.232C8.417 2.175 8.796 2.163 12 2.163V2.163ZM12 0C8.741 0 8.333 0.014 7.053 0.072C2.695 0.272 0.273 2.69 0.073 7.052C0.014 8.333 0 8.741 0 12C0 15.259 0.014 15.668 0.072 16.948C0.272 21.306 2.69 23.728 7.052 23.928C8.333 23.986 8.741 24 12 24C15.259 24 15.668 23.986 16.948 23.928C21.302 23.728 23.73 21.31 23.927 16.948C23.986 15.668 24 15.259 24 12C24 8.741 23.986 8.333 23.928 7.053C23.732 2.699 21.311 0.273 16.949 0.073C15.668 0.014 15.259 0 12 0V0ZM12 5.838C8.597 5.838 5.838 8.597 5.838 12C5.838 15.403 8.597 18.163 12 18.163C15.403 18.163 18.162 15.404 18.162 12C18.162 8.597 15.403 5.838 12 5.838ZM12 16C9.791 16 8 14.21 8 12C8 9.791 9.791 8 12 8C14.209 8 16 9.791 16 12C16 14.21 14.209 16 12 16ZM18.406 4.155C17.61 4.155 16.965 4.8 16.965 5.595C16.965 6.39 17.61 7.035 18.406 7.035C19.201 7.035 19.845 6.39 19.845 5.595C19.845 4.8 19.201 4.155 18.406 4.155Z"
                         fill="black"/>
@@ -309,7 +309,7 @@ export class MainApp extends Component {
                 </a>
                 <a href={"https://www.facebook.com/alinahairstylistedmonds/"} target={"_blank"}>
                   <svg className={"media-icon"} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <g clip-path="url(#clip0_0_1)">
+                    <g clipPath="url(#clip0_0_1)">
                       <path
                         d="M22.675 0H1.325C0.593 0 0 0.593 0 1.325V22.676C0 23.407 0.593 24 1.325 24H12.82V14.706H9.692V11.084H12.82V8.413C12.82 5.313 14.713 3.625 17.479 3.625C18.804 3.625 19.942 3.724 20.274 3.768V7.008L18.356 7.009C16.852 7.009 16.561 7.724 16.561 8.772V11.085H20.148L19.681 14.707H16.561V24H22.677C23.407 24 24 23.407 24 22.675V1.325C24 0.593 23.407 0 22.675 0V0Z"
                         fill="black"/>
@@ -329,7 +329,7 @@ export class MainApp extends Component {
         </section>
 
         <div className={"main-background half-page"}>
-          <div className="left-shapes">
+          <div className="left-shapes mobile-visible">
             <svg width="20vmin" height="20vmin" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
               <g id="Stars">
                 <path id="Star 1"
@@ -352,7 +352,7 @@ export class MainApp extends Component {
               </g>
             </svg>
           </div>
-          <div className="right-shapes">
+          <div className="right-shapes mobile-visible">
             <svg width="20vmin" height="10vmin" viewBox="0 0 100 50" fill="none" xmlns="http://www.w3.org/2000/svg">
               <g id="Small Squiggles">
                 <path id="Squiggle Small"
--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -2,24 +2,48 @@ import React, {Component} from 'react';
 import './Components.css';
 import './Grid.css';
 
+// NOT an actual deep copy, but since we just use it for lists that contain
+// strings, it should be ok. BE WARNED: THIS FUNCTION IS DANGEROUS!
+function fakeDeepCopy(obj) {
+  return JSON.parse(JSON.stringify(obj));
+}
+
 export class Gallery extends Component {
+  state = {
+    images: [],
+  }
+
   componentDidMount() {
-    const BASE_URL = "https://github.com/StefanTodoran/alina-hairstylist/tree/main/src/assets/services";
+    this.loadImages().then((images) => {
+      this.setState({ images: images })
+    });
+  }
+
+  async loadImages() {
+    const BASE_URL = "https://raw.githubusercontent.com/StefanTodoran/alina-hairstylist/main/src/assets/services/";
+    const images = [];
+    for (let i = 1; i < 19; i++) {
+      const url = BASE_URL + `${i}.jpeg`;
+      // console.log(i, url);
+      const response = await fetch(url);
+      if (response.ok) {
+        // console.log('OK!')
+        images.push(url);
+      }
+    }
+    return images;
   }
 
   render() {
-        return (
-            <section className={"gallery-grid"}>
-                <div className={"gallery-card"} id={"card-1"}/>
-                <div className={"gallery-card"} id={"card-2"}/>
-                <div className={"gallery-card"} id={"card-3"}/>
-                <div className={"gallery-card"} id={"card-4"}/>
-                <div className={"gallery-card"} id={"card-5"}/>
-                <div className={"gallery-card"} id={"card-6"}/>
-                <div className={"gallery-card"} id={"card-7"}/>
-                <div className={"gallery-card"} id={"card-8"}/>
-                <div className={"gallery-card"} id={"card-9"}/>
-            </section>
-        );
+    const images = [];
+    for (let i = 0; i < this.state.images.length; i++) {
+      images.push(<div className={"gallery-card"} style={{"--src": `url(${this.state.images[i]})`}}/>);
     }
+
+    return (
+      <section className={"gallery-grid"}>
+        {images}
+      </section>
+    );
+  }
 }
